0fd0e50f638b5b84c468c95f37ff04ff5cc6d4fe
marie-hotchkiss <mtknowlton67@gmail.com>
2020-08-10 10:45:57 -0700

    feat(created basic server :beer:

--- data/server.js
+++ data/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+const morgan = require("morgan");
 //const RecipeRouter = require("./recipes/recipe-router");
 
 const server = express();
-
+server.use(morgan("dev"));
 server.use(express.json());
 //server.use("/api/recipes", RecipeRouter);
 
